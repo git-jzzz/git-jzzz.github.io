7d73115535c9eeb0582edd90115d2706ba13962d
git-jzzz <root_jz@163.com>
2021-01-16 16:28:54 +0800

    提交文件

--- public/js/src/FunnyTitle.js
+++
@@ -1,3 +0,0 @@
-// build time:Wed Dec 02 2020 18:13:25 GMT+0800 (GMT+08:00)
-var OriginTitle=document.title;var titleTime;document.addEventListener("visibilitychange",function(){if(document.hidden){$('[rel="icon"]').attr("href","/images/funny.ico");document.title="┭┮﹏┭┮ 电脑崩溃啦！！！！！";clearTimeout(titleTime)}else{$('[rel="icon"]').attr("href","/images/favicon.ico");document.title="诶嘿 又好啦(#^.^#)"+OriginTitle;titleTime=setTimeout(function(){document.title=OriginTitle},2e3)}});
-//rebuild by neat 
--- public/js/src/copyHtml.js
+++
@@ -1,3 +0,0 @@
-// build time:Wed Dec 02 2020 18:13:25 GMT+0800 (GMT+08:00)
-
-//rebuild by neat 
