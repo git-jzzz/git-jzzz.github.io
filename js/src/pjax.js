33a2af932524ff429a9a286ca319a30a7d388098
git-jzzz <root_jz@163.com>
2021-01-16 16:27:05 +0800

    Site updated: 2021-01-16 16:26:40

--- js/src/pjax.js
+++ js/src/pjax.js
@@ -1,3 +1,3 @@
-// build time:Sat Jan 16 2021 15:45:56 GMT+0800 (GMT+08:00)
+// build time:Sat Jan 16 2021 16:24:14 GMT+0800 (GMT+08:00)
 var pjax=new Pjax({elements:"a",selectors:["title","#pjax-container"]});
 //rebuild by neat 
